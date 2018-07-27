b76948465f919de29cf3a8ca08cb533405b67297
Matthieu <lawn_street@yahoo.com>
2018-07-27 16:46:50 +0200

    ajout repo.php

---
+++ Repophp.php
@@ -0,0 +1,46 @@
+<?php
+
+echo "Helloworld";
+
+$nom = 'Thomas';
+
+echo "$nom";
+
+echo "<br>" . $nom;
+
+$array = [ 1, 2, 3, 4, 5 ];
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>PHP</title>
+    <link rel="stylesheet" href="order-form.css">
+</head>
+<body>
+    <section>
+        <h1>Bons de commande</h1>
+
+        <table class="standard-table">
+            <caption>Liste des commandes</caption>
+            <thead>
+                <tr>
+                    <th>Commande</th>
+                    <th>Date de la commande</th>
+                    <th>Date de livraison</th>
+                    <th>Statut</th>
+                </tr>
+            </thead>
+            <tbody>
+                    </tr>
+
+
+            </tbody>
+        </table>
+    </section>
+</body>
+</html>
